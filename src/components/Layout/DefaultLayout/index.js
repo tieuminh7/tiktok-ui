bb6d729d64b895d9b8634015c1896d7766513e9e
tieuminh7 <tieuminh.flancerwork@gmail.com>
2024-08-08 20:41:23 +0700

    PUSH3: DỰNG LAYOUT

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
 import Header from "../components/Header";
+import classNames from "classnames/bind";
 import Sidebar from "./Sidebar";
+import styles from "./DefaultLayout.module.scss";
 
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx("wrapper")}>
       <Header />
-      <div className="container"></div>
-      <Sidebar />
-      <div className="content">{children}</div>
+      <div className={cx("container")}>
+        <Sidebar />
+        <div className={cx("content")}>{children}</div>
+      </div>
     </div>
   );
 }
